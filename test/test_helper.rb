366a54b7b7ec3f66b70f1fc43065251014137e4f
Geoffrey Hichborn <geoff@socialcast.com>
2011-10-24 13:41:44 -0700

    Updated to work with Rails 3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'test/unit'
 require 'active_record'
-require File.join(File.dirname(__FILE__), "../rails/init")
+require File.join(File.dirname(__FILE__), "../init")
 require 'has-bit-field'
 
 #ActiveRecord::Base.logger = Logger.new($stdout)
